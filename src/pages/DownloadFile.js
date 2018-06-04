08c897b9ca35aab5ddc6fca329a4a57a140a5e42
人工智慧實驗室 <lab507@rengongzhihuishiyanshide-MacBook-Air-2.local>
2018-06-04 21:03:34 +0800

    0704_first

--- src/pages/DownloadFile.js
+++ src/pages/DownloadFile.js
@@ -3,12 +3,16 @@ import { connect } from 'react-redux'
 import {
   Text,
   View,
+  FlatList,
+  ActivityIndicator,
 } from 'react-native'
 import PropTypes from 'prop-types'
 import styles from './styles/DownloadFile.styles'
 import navAction from '../actions/nav.action'
 import CloseImage from '../../asset/close.png'
 import Appbar from '../components/Appbar'
+import FileItem from '../components/FileItem'
+import mockDownloadData from '../../asset/mockDownloadData.json'
 
 
 const mapStateToProps = state => ({
@@ -23,15 +27,68 @@ const mapDispatchToProps = dispatch => ({
 })
 
 class DownloadFile extends Component {
+  constructor() {
+    super()
+    this.state = {
+      showActivityIndicator: false,
+      downloadData: mockDownloadData.Files,
+    }
+    this.FileOnPress = this.FileOnPress.bind(this)
+  }
+
+  FileOnPress = (pressedItem) => {
+    const FileData = this.state.downloadData
+    const itemIndex = FileData.findIndex(item => item === pressedItem)
+    if (FileData[itemIndex].State === '未下載') {
+      this.setState({ showActivityIndicator: !this.state.showActivityIndicator })
+      setTimeout(() => this.setState({ showActivityIndicator: false }), 3000)
+      setTimeout(() => {
+        FileData[itemIndex].State = '已下載'
+        this.setState({ downloadData: FileData })
+      }, 3000)
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Appbar title='檔案下載' withDrawer
           rightIcon={CloseImage}
           onRightPress={this.props.navAction.onExit}/>
-        <View style={styles.rowContainer}>
-          <Text style={styles.text}>歡迎進入檔案下載</Text>
-        </View>
+        { (this.state.downloadData === undefined) ? (
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>
+              目前已上傳檔案是空的QQ
+            </Text>
+          </View>
+        ) : (
+          <View style={styles.listContainer}>
+            <FlatList
+              style={styles.list}
+              data={[...this.state.downloadData].reverse()}
+              keyExtractor={ (item, index) => index.toString()}
+              renderItem={({ item }) => (
+                <FileItem
+                  FileName={item.FileName}
+                  FileDescription={item.FileDescription}
+                  State={item.State}
+                  UploadTime={item.UploadTime}
+                  onPress={() => { this.FileOnPress(item) }}
+                />
+              )}
+            />
+            {this.state.showActivityIndicator === true && (
+              <View style={styles.OverlayContainer}>
+                <View style={styles.activityIndicatorContainer}>
+                  <ActivityIndicator style={styles.activityIndicator} size="large" color="#3A8FB7" />
+                </View>
+                <Text style={styles.activityIndicatorText}>
+                  下載檔案中...{'\n'}請稍候
+                </Text>
+              </View>
+            )}
+          </View>
+        )}
       </View>
     )
   }
@@ -47,4 +104,3 @@ DownloadFile.propTypes = {
 }
 //  connect react component & redux store
 export default connect(mapStateToProps, mapDispatchToProps)(DownloadFile)
-
