37b7912b708f39a89ea7d695d64d089d991b5067
EELab507 <num1@EELab507deMacBook-Air.local>
2018-06-17 22:23:57 +0800

    add src/components/ResultItemData.js

---
+++ src/components/ResultItemData.js
@@ -0,0 +1,22 @@
+const ResultItemData = [
+  {
+    id: 0,
+    title: ['統計結果'],
+    user: 'teacher',
+    routeName: 'SingleResult',
+  },
+  {
+    id: 1,
+    title: ['統計結果'],
+    user: 'teacher',
+    routeName: 'MultiResult',
+  },
+  {
+    id: 2,
+    title: ['統計結果'],
+    user: 'teacher',
+    routeName: 'TrueFalseResult',
+  },
+]
+
+export default ResultItemData
