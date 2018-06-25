f805ea643ee9a6af388ddcbd79fd235015dc1e0e
Benjamin Tsai <bentsai@Benjamins-MacBook-Pro.local>
2018-06-25 22:26:03 +0800

    Middleware Restore

--- src/util/middlewares.js
+++ src/util/middlewares.js
@@ -1,5 +1,6 @@
 import { Alert } from 'react-native'
 import { createReactNavigationReduxMiddleware } from 'react-navigation-redux-helpers'
+import MultiPeerActions from '../actions/multiPeer.action'
 // import drawLotsAction from '../actions/drawLots.action'
 // import Background from '../components/Background'
 
@@ -24,13 +25,24 @@ const messageMiddleware = ({ dispatch, getState }) => (
     (action) => {
       if (action.type === 'multiPeer/backend/onDataReceived') {
         const { data } = action.payload
-        if (data.messageType === 'CHOSEN_ONE') {
-          // dispatch(drawLotsAction.setChosen(data.textPop))
+        switch (data.messageType) {
+        case 'REQUEST_INFO':
+          dispatch(MultiPeerActions.backend.returnInfo(action.senderId, {
+            name: getState().multipeer.selfName,
+          }))
+          break
+        case 'RETURN_INFO':
+          dispatch(MultiPeerActions.backend.onInfoUpdate(action.senderId, action.data.info))
+          break
+        case 'CHOSEN_ONE':
           Alert.alert(
             '您被老師抽到要',
             data.textPop,
             [{ text: 'OK' }],
           )
+          break
+        default:
+          break
         }
       }
       return next(action)
